f839fef9f855216210996ad96113b50bddc0e10f
Alex Sanmartín <isanmartin8a@gmail.com>
2023-01-11 13:01:25 +0100

    reforma

---
+++ static/js/draggable.js
@@ -0,0 +1,49 @@
+    // // // // Make the DIV element draggable:
+    dragElement(document.getElementsByClassName("lightWindow"));
+    
+    function dragElement(elmnt) {
+        $(elmnt).css("transition", "0s");
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementsByClassName(elmnt.id)) {
+            // if present, the header is where you move the DIV from:
+            console.log("hika");
+            document.getElementsByClassName(elmnt.id).onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
+        }
+        
+        function dragMouseDown(e) {
+            $(elmnt).css("transition", "0s");
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            $(elmnt).css("transition", "0s");
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            console.log("hika");
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+            $(elmnt).css("transition", "0.5s");
+        }
+    }
---
+++ static/js/settings.js
@@ -0,0 +1,19 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+document.getElementsByTagName('head')[0].appendChild(script);
+
+$('.lightButSets').click(function() {
+    $('.lightWindow').toggleClass("showWindow");
+    });
+
+  $('.mode').click(function() {
+    $('body').toggleClass("dark");
+    $(this).toggleClass("active");
+    $('.lightSets').toggleClass("darkSets");
+    $('.lightWindow').toggleClass("darkWindow");
+    $('.lightButSets').toggleClass("darkButSets");
+  });
+
+  $('.language').click(function() {
+    $(this).toggleClass("langActive");
+  });
