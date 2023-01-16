56eb2507bd8603d93750e67d34ad739741659c02
Alex Sanmartín <isanmartin8a@gmail.com>
2023-01-16 14:14:56 +0100

    cambios

--- static/js/draggable.js
+++ static/js/draggable.js
@@ -1,49 +1,74 @@
-    // // // // Make the DIV element draggable:
-    dragElement(document.getElementsByClassName("lightWindow"));
-    dragElement(document.getElementsByClassName("darkWindow"));
-    dragElement(document.getElementsByClassName("showWindow"));
-    
-    function dragElement(elmnt) {
-        $(elmnt).css("transition", "0s");
-        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-        elmnt.onMouseDown = function() { alert("Hello World"); };
-        console.log("empiezo para draggear");
-        
-        function dragMouseDown(e) {
-            alert("hola");
-            console.log("clicko y trackeo el cursor");
-            $(elmnt).css("transition", "0s");
-            e = e || window.event;
-            e.preventDefault();
-            // get the mouse cursor position at startup:
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            document.onmouseup = closeDragElement;
-            // call a function whenever the cursor moves:
-            document.onmousemove = elementDrag;
-        }
-        
-        function elementDrag(e) {
-            console.log("marco la nueva posicion");
-            $(elmnt).css("transition", "0s");
-            e = e || window.event;
-            e.preventDefault();
-            // calculate the new cursor position:
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            // set the element's new position:
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-            console.log("hika");
-        }
+// // // // Make the DIV element draggable:
+dragElement(document.getElementsByClassName("lightWindow"));
+dragElement(document.getElementsByClassName("darkWindow"));
+dragElement(document.getElementsByClassName("showWindow"));
 
-        function closeDragElement() {
-            // stop moving when mouse button is released:
-            document.onmouseup = null;
-            document.onmousemove = null;
-            $(elmnt).css("transition", "0.5s");
-        }
+function dragElement(elmnt) {
+  $(elmnt).css("transition", "0s");
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  elmnt.onMouseDown = function () {
+    alert("Hello World");
+  };
+  console.log("empiezo para draggear");
+
+  function dragMouseDown(e) {
+    alert("hola");
+    console.log("clicko y trackeo el cursor");
+    $(elmnt).css("transition", "0s");
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    console.log("marco la nueva posicion");
+    $(elmnt).css("transition", "0s");
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    console.log("hika");
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+    $(elmnt).css("transition", "0.5s");
+  }
+}
+
+function tamanoVentanaNavegador() {
+    var dimensiones = [];
+    if (typeof (window.innerWidth) == 'number') {
+        dimensiones = [window.innerWidth, window.innerHeight];
+    } else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+        dimensiones = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+    } else if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+        dimensiones = [document.body.clientWidth, document.body.clientHeight];
     }
+    return dimensiones;
+}
 
+var dimensiones = tamanoVentanaNavegador();
+var width = parseFloat(dimensiones[0])/2;
+var height = parseFloat(dimensiones[1])/2;
+  
+  if(window.matchMedia("(max-width: 700px)")){
+    $(".lightWindow").css("top", height - 80) ;
+    $(".lightWindow").css("left", width - 48);
+  }
--- static/js/settings.js
+++ static/js/settings.js
@@ -1,43 +1,64 @@
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
+var script = document.createElement("script");
+script.src = "https://code.jquery.com/jquery-3.6.3.min.js"; // Check https://jquery.com/ for the current version
+document.getElementsByTagName("head")[0].appendChild(script);
 
-$('.lightButSets').click(function() {
-    $('.lightWindow').toggleClass("showWindow").css("transition", "0.5s");
-    });
+$(".lightButSets").click(function () {
+  $(".lightWindow").toggleClass("showWindow").css("transition", "0.5s");
+});
 
-  $('.mode').click(function() {
-    $('body').toggleClass("dark");
-    $(this).toggleClass("active");
-    $('.lightSets').toggleClass("darkSets");
-    $('.lightWindow').toggleClass("darkWindow");
-    $('.lightButSets').toggleClass("darkButSets");
-    $('.lightMenu').toggleClass("darkMenu");
-    $('.lightScratch').toggleClass("darkScratch");
-    $('.lightCloud').toggleClass("darkCloud");
-    $('.icon').toggleClass("darkIcon");
-  });
+$(".mode").click(function () {
+  $("body").toggleClass("dark");
+  $(this).toggleClass("active");
+  $(".lightSets").toggleClass("darkSets");
+  $(".lightWindow").toggleClass("darkWindow");
+  $(".lightButSets").toggleClass("darkButSets");
+  $(".lightMenu").toggleClass("darkMenu");
+  $(".lightScratch").toggleClass("darkScratch");
+  $(".lightCloud").toggleClass("darkCloud");
+  $(".icon").toggleClass("darkIcon");
+});
 
-  $('.language').click(function() {
-    $(this).toggleClass("langActive");
-  });
+$(".language").click(function () {
+  $(this).toggleClass("langActive");
+});
 
-  $('.home').click(function() {
-    $('.first').addClass("activateChapter");
-    $('.second').removeClass("activateChapter");
-    $('#third').css("opacity", 0);
-    $('#fourth').css("opacity", 0);
-  });
+$(".home").click(function () {
+  $(".first").addClass("activateChapter");
+  $(".second").removeClass("activateChapter");
+  $("#third").css("opacity", 0);
+  $("#fourth").css("opacity", 0);
+});
 
-  $('.projects').click(function() {
-    $('.first').removeClass("activateChapter");
-    $('.second').addClass("activateChapter");
-    $('#third').css("opacity", 0);
-    $('#fourth').css("opacity", 0);
-  });
+$(".projects").click(function () {
+  $(".first").removeClass("activateChapter");
+  $(".second").addClass("activateChapter");
+  $("#third").css("opacity", 0);
+  $("#fourth").css("opacity", 0);
+});
 
-  window.onload = function() {
-    setTimeout(function() {
-      $('.first').addClass("activateChapter");
-    }, 300);
-  }
+window.onload = function () {
+  setTimeout(function () {
+    $(".first").addClass("activateChapter");
+  }, 100);
+};
+
+function cloudTransition() {
+  $("#cloud").css("animation", "cloudIn 2.5s ease");
+  $(".first").removeClass("activateChapter");
+  $("#menu").css("display", "none");
+  setTimeout(function () {
+    $("#summary").addClass("activateChapter");
+  }, 1000);
+}
+
+// $("#chatterInfo").css("left", $("#chatter").css("right"));
+// $("#radarfiInfo").css("left", $("#radarfi").css("right"));
+$("#chatterInfo").css("left", "10rem");
+$(document).on("mousemove",".projectCard",function(e){ 
+
+  var y = e.clientY;  
+  var chatterY = y - 440; 
+  $("#chatterInfo").css("transform","translate3d("+"0,"+chatterY+"px,0px)");
+  var radarfiY = y - 706; 
+  $("#radarfiInfo").css("transform","translate3d(" + "0,"+radarfiY+"px,0px)");
+});
