5320de61430f1478f17914164298cff851f49aa6
Alex Sanmartín <isanmartin8a@gmail.com>
2023-01-20 13:57:51 +0100

    sobre mi

--- static/js/draggable.js
+++ static/js/draggable.js
@@ -1,19 +1,19 @@
-import gsap from "https://cdn.skypack.dev/gsap";
-import splitting from "https://cdn.skypack.dev/splitting";
+// import gsap from "https://cdn.skypack.dev/gsap";
+// import splitting from "https://cdn.skypack.dev/splitting";
 
-const anchors = document.querySelectorAll("span");
+// const anchors = document.querySelectorAll("span");
 
-for (const anchor of anchors) {
-  const res = splitting({
-    target: anchor,
-    by: "spans",
-  });
-  const icon = anchor.querySelector("svg");
-  for (const char of [...res[0].spans, icon]) {
-    if (span) {
-      span.style.setProperty("--r", gsap.utils.random(-15, 15, 1));
-      span.style.setProperty("--y", gsap.utils.random(-50, 25, 1));
-      span.style.setProperty("--x", gsap.utils.random(-25, 25, 1));
-    }
-  }
-}
+// for (const anchor of anchors) {
+//   const res = splitting({
+//     target: anchor,
+//     by: "spans",
+//   });
+//   const icon = anchor.querySelector("svg");
+//   for (const char of [...res[0].spans, icon]) {
+//     if (span) {
+//       span.style.setProperty("--r", gsap.utils.random(-15, 15, 1));
+//       span.style.setProperty("--y", gsap.utils.random(-50, 25, 1));
+//       span.style.setProperty("--x", gsap.utils.random(-25, 25, 1));
+//     }
+//   }
+// }
